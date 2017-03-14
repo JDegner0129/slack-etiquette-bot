001fbcf25a25945fa0e1987b1cd57a78cfcdfe8a
Jordan Degner <jdegner0129@gmail.com>
2017-03-14 12:29:45 -0700

    Initial implementation of Slack etiquette bot

---
+++ index.js
@@ -0,0 +1,41 @@
+const { WebClient, RtmClient, MemoryDataStore, RTM_EVENTS, CLIENT_EVENTS } = require('@slack/client');
+
+const token = process.env.SLACK_API_TOKEN || '';
+const channelMemberThreshold = process.env.CHANNEL_MEMBER_THRESHOLD || 20;
+
+const rtm = new RtmClient(token, { logLevel: 'error', dataStore: new MemoryDataStore() });
+const web = new WebClient(token);
+
+rtm.start();
+
+const trySendMessageToChannel = ({ id, members }) => {
+  if (!members || members.length < channelMemberThreshold) {
+    return;
+  }
+
+  rtm.sendMessage(
+    `:wave: Hey there! You just used \`@here\` or \`@channel\` in a channel with ${members.length} members. Next time, consider giving your message a few minutes without the tag before tagging a large group (and maybe tag specific people!). :slightly_smiling_face:`,
+    id
+  );
+};
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, () => {
+  console.log('RTM client authenticated!');
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, ({ user, channel, text }) => {
+  if (!(text.includes('<!here|@here>') || text.includes('<!channel>'))) {
+    return;
+  }
+
+  let channelInfo = rtm.dataStore.getChannelById(channel);
+  if (!channelInfo) {
+    web.channels.info(channel).then(info => {
+      channelInfo = info.channel;
+      rtm.dataStore.setChannel(channelInfo);
+      trySendMessageToChannel(channelInfo);
+    });
+  } else {
+    trySendMessageToChannel(channelInfo);
+  }
+});
